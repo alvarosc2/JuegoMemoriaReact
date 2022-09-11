af3229a2fc7f0c2ad1ba4a280d372a4c93ac9f21
alvarosc2 <alvarosc2@gmail.com>
2022-09-11 11:12:08 -0500

    Styling the cards.

--- juego-memoria-react/src/index.js
+++ juego-memoria-react/src/index.js
@@ -4,15 +4,46 @@ import {MoonCard} from './MoonCard.js'
 import {StarCard} from './StarCard.js'
 import {SunCard} from './SunCard.js'
 import {CometCard} from './CometCard.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
 
 
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
 root.render(<div>
- <MoonCard />
-    <StarCard />
-    <SunCard />
-    <CometCard />
+<Button/>
+
+<div className="container">
+  <div className="row">
+    <div className="col">
+        <MoonCard />
+    </div>
+    <div className="col">
+        <StarCard />
+    </div>
+    <div className="col">
+        <SunCard />
+    </div>
+    <div className="col">
+        <CometCard />
+    </div>
+  </div>
+
+  <div className="row">
+    <div className="col">
+        <MoonCard />
+    </div>
+    <div className="col">
+        <StarCard />
+    </div>
+    <div className="col">
+        <SunCard />
+    </div>
+    <div className="col">
+        <CometCard />
+    </div>
+  </div>
+</div>
  
 </div>)
